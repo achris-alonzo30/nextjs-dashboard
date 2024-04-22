151e2ff8c95e1c503afae1fb9d2e3cf13b969fb2
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-04-22 16:50:49 -0300

    1: initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import {Poppins, Lato } from "next/font/google";
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const lato = Lato({
+    subsets: ["latin"],
+    weight: ["100", "300", "400", "700", "900"],
+});
+
+
+
